391049a62c3330463d876062543e33bae943a1b1
Dave Gines <dgines@rentdynamics.com>
2023-06-18 00:26:38 -0600

    Add DI container

--- EstimatedArrivalTime/Program.cs
+++ EstimatedArrivalTime/Program.cs
@@ -1,4 +1,15 @@
 ﻿using EstimatedArrivalTime.RulesEngineSolution;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-ArrivalTimePrediction arrivalTimePrediction = new();
-arrivalTimePrediction.Run();
+using IHost host = Host.CreateDefaultBuilder(args)
+  .ConfigureServices(services =>
+  {
+    services.AddSingleton<ArrivalTimePrediction>();
+  })
+  .Build();
+
+var arrivalTimePrediction = host.Services.GetService<ArrivalTimePrediction>();
+arrivalTimePrediction!.Run();
+
+await host.RunAsync();
